0974bc632085d8d27a224b84059b11137a86c2a8
dawidl022 <domdaw7@gmail.com>
2021-08-09 19:55:58 +0100

    Add working image gallery

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,5 +1,24 @@
+
 function activateGallery() {
-  alert("Hello from the gallery file!");
+  const thumbnails = document.querySelectorAll("#gallery-thumbs img");
+  const mainImage = document.querySelector("#gallery-photo > img");
+  const imageTitle = document.querySelector(".gallery-info .title");
+  const imageDescription = document.querySelector(".gallery-info .description");
+
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+      mainImage.src = thumbnail.dataset.largeVersion;
+      mainImage.alt = thumbnail.alt;
+      imageTitle.textContent = thumbnail.dataset.title;
+      imageDescription.textContent = thumbnail.dataset.description;
+
+      const currentClass = "current";
+      document.querySelector("." + currentClass).classList.remove(currentClass);
+
+      thumbnail.parentElement.classList.add("current");
+
+    });
+  });
 }
 
 activateGallery();
